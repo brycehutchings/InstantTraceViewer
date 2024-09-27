2f65a2b5b8a0d6d18b90e81dec39db4ac0caa025
Bryce Hutchings <bryceh@microsoft.com>
2024-09-27 13:32:36 -0700

    more kernel improvements

--- src/InstantTraceViewerUI/Etw/EtwTraceSource.KernelEventHandlers.cs
+++ src/InstantTraceViewerUI/Etw/EtwTraceSource.KernelEventHandlers.cs
@@ -1,10 +1,135 @@
-using Microsoft.Diagnostics.Tracing;
+﻿using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using System;
 
 namespace InstantTraceViewerUI.Etw
 {
     internal partial class EtwTraceSource : ITraceSource
     {
+        // Copied from Microsoft.Diagnostics.Tracing.Parsers.Kernel in order to improve naming.
+        [Flags]
+        public enum KernelFileCreateOptions
+        {
+            None = 0,
+            Archive = 0x20,
+            Compressed = 0x800,
+            Device = 0x40,
+            Directory = 0x10,
+            Encrypted = 0x4000,
+            Hidden = 2,
+            IntegrityStream = 0x8000,
+            Normal = 0x80,
+            NotContentIndexed = 0x2000,
+            NoScrubData = 0x20000,
+            Offline = 0x1000,
+            ReadOnly = 1,
+            ReparsePoint = 0x400,
+            SparseFile = 0x200,
+            System = 4,
+            Temporary = 0x100,
+            Virtual = 0x10000
+        }
+
+        // Copied from Microsoft.Diagnostics.Tracing.Parsers.Kernel in order to improve naming.
+        public enum KernelFileCreateDisposition
+        {
+            Supersede = 0,
+            CreateNew = 2,
+            CreateAlways = 5,
+            OpenExisting = 1,
+            OpenAlways = 3,
+            TruncateExisting = 4
+        }
+
+        // Copied from https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/ne-wdm-_file_information_class
+        private enum FileInformationClass
+        {
+            FileDirectoryInformation = 1,
+            FileFullDirectoryInformation = 2,
+            FileBothDirectoryInformation = 3,
+            FileBasicInformation = 4,
+            FileStandardInformation = 5,
+            FileInternalInformation = 6,
+            FileEaInformation = 7,
+            FileAccessInformation = 8,
+            FileNameInformation = 9,
+            FileRenameInformation = 10,
+            FileLinkInformation = 11,
+            FileNamesInformation = 12,
+            FileDispositionInformation = 13,
+            FilePositionInformation = 14,
+            FileFullEaInformation = 15,
+            FileModeInformation = 16,
+            FileAlignmentInformation = 17,
+            FileAllInformation = 18,
+            FileAllocationInformation = 19,
+            FileEndOfFileInformation = 20,
+            FileAlternateNameInformation = 21,
+            FileStreamInformation = 22,
+            FilePipeInformation = 23,
+            FilePipeLocalInformation = 24,
+            FilePipeRemoteInformation = 25,
+            FileMailslotQueryInformation = 26,
+            FileMailslotSetInformation = 27,
+            FileCompressionInformation = 28,
+            FileObjectIdInformation = 29,
+            FileCompletionInformation = 30,
+            FileMoveClusterInformation = 31,
+            FileQuotaInformation = 32,
+            FileReparsePointInformation = 33,
+            FileNetworkOpenInformation = 34,
+            FileAttributeTagInformation = 35,
+            FileTrackingInformation = 36,
+            FileIdBothDirectoryInformation = 37,
+            FileIdFullDirectoryInformation = 38,
+            FileValidDataLengthInformation = 39,
+            FileShortNameInformation = 40,
+            FileIoCompletionNotificationInformation = 41,
+            FileIoStatusBlockRangeInformation = 42,
+            FileIoPriorityHintInformation = 43,
+            FileSfioReserveInformation = 44,
+            FileSfioVolumeInformation = 45,
+            FileHardLinkInformation = 46,
+            FileProcessIdsUsingFileInformation = 47,
+            FileNormalizedNameInformation = 48,
+            FileNetworkPhysicalNameInformation = 49,
+            FileIdGlobalTxDirectoryInformation = 50,
+            FileIsRemoteDeviceInformation = 51,
+            FileUnusedInformation = 52,
+            FileNumaNodeInformation = 53,
+            FileStandardLinkInformation = 54,
+            FileRemoteProtocolInformation = 55,
+            FileRenameInformationBypassAccessCheck = 56,
+            FileLinkInformationBypassAccessCheck = 57,
+            FileVolumeNameInformation = 58,
+            FileIdInformation = 59,
+            FileIdExtdDirectoryInformation = 60,
+            FileReplaceCompletionInformation = 61,
+            FileHardLinkFullIdInformation = 62,
+            FileIdExtdBothDirectoryInformation = 63,
+            FileDispositionInformationEx = 64,
+            FileRenameInformationEx = 65,
+            FileRenameInformationExBypassAccessCheck = 66,
+            FileDesiredStorageClassInformation = 67,
+            FileStatInformation = 68,
+            FileMemoryPartitionInformation = 69,
+            FileStatLxInformation = 70,
+            FileCaseSensitiveInformation = 71,
+            FileLinkInformationEx = 72,
+            FileLinkInformationExBypassAccessCheck = 73,
+            FileStorageReserveIdInformation = 74,
+            FileCaseSensitiveInformationForceAccessCheck = 75,
+            FileKnownFolderInformation = 76,
+            FileStatBasicInformation = 77,
+            FileId64ExtdDirectoryInformation = 78,
+            FileId64ExtdBothDirectoryInformation = 79,
+            FileIdAllExtdDirectoryInformation = 80,
+            FileIdAllExtdBothDirectoryInformation = 81,
+            FileStreamReservationInformation,
+            FileMupProviderInfo,
+            FileMaximumInformation
+        }
+
         private void SubscribeToKernelEvents()
         {
             //
@@ -65,7 +190,7 @@ namespace InstantTraceViewerUI.Etw
             if (!string.IsNullOrEmpty(obj.FileName))
             {
                 var newRecord = CreateBaseTraceRecord(obj);
-                newRecord.Message = $"File:{obj.FileName} CreateOptions:{obj.CreateOptions} CreateDisposition:{obj.CreateDisposition} FileAttributes:{obj.FileAttributes} ShareAccess:{obj.ShareAccess}"; // IrpPtr:{obj.IrpPtr} FileObject:{obj.FileObject}";
+                newRecord.Message = $"File:{obj.FileName} CreateOptions:{(KernelFileCreateOptions)obj.CreateOptions} CreateDisposition:{(KernelFileCreateDisposition)obj.CreateDisposition} FileAttributes:{obj.FileAttributes} ShareAccess:{obj.ShareAccess}"; // IrpPtr:{obj.IrpPtr} FileObject:{obj.FileObject}";
                 AddEvent(newRecord);
             }
         }
@@ -113,7 +238,13 @@ namespace InstantTraceViewerUI.Etw
             {
                 // TODO: How do we parse the IO request packet flags (obj.IoFlags)?
                 var newRecord = CreateBaseTraceRecord(obj);
-                newRecord.Message = $"File:{obj.FileName} Offset:{obj.Offset} IoSize:{obj.IoSize}"; // IrpPtr:{obj.IrpPtr} FileKey:{obj.FileKey}";
+                newRecord.Message = $"File:{obj.FileName} Offset:{obj.Offset} Size:{obj.IoSize}"; // IrpPtr:{obj.IrpPtr} FileKey:{obj.FileKey}";
+
+                if (obj.IoFlags != 0)
+                {
+                    newRecord.Message += $" IoFlags:{obj.IoFlags}";
+                }
+
                 AddEvent(newRecord);
             }
         }
@@ -134,7 +265,17 @@ namespace InstantTraceViewerUI.Etw
             if (!string.IsNullOrEmpty(obj.FileName))
             {
                 var newRecord = CreateBaseTraceRecord(obj);
-                newRecord.Message = $"File:{obj.FileName} InfoClass={obj.InfoClass} ExtraInfo={obj.ExtraInfo}"; // IrpPtr:{obj.IrpPtr} FileKey:{obj.FileKey}";
+                newRecord.Message = $"File:{obj.FileName} InfoClass={(FileInformationClass)obj.InfoClass}"; // IrpPtr:{obj.IrpPtr} FileKey:{obj.FileKey}";
+
+                if (obj.InfoClass == (int)FileInformationClass.FileEndOfFileInformation)
+                {
+                    newRecord.Message += $" EndOfFilePosition:{obj.ExtraInfo}";
+                }
+                else if (obj.ExtraInfo != 0)
+                {
+                    newRecord.Message += $" ExtraInfo:{obj.ExtraInfo}";
+                }
+
                 AddEvent(newRecord);
             }
         }
