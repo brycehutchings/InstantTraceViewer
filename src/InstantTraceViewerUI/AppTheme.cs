c105294d11e3df289ae124141bba554464ecfe8e
Bryce Hutchings <bryceh@microsoft.com>
2025-09-18 16:48:13 -0700

    Switch to custom palette

--- src/InstantTraceViewerUI/AppTheme.cs
+++ src/InstantTraceViewerUI/AppTheme.cs
@@ -16,8 +16,10 @@ namespace InstantTraceViewerUI
     // Color is computed using HSV where the hue is varied in 30 degree increments (12 * 30 = 360). This must happen at render time because the theme can change.
     public enum HighlightRowBgColor
     {
-        _1, _2, _3, _4, _5, _6,
-        _7, _8, _9, _10, _11, _12
+        // https://sashamaps.net/docs/resources/20-colors/
+        Maroon, Brown, Olive, Teal, Navy, Black,
+        Red, Orange, Yellow, Lime, Green, Cyan, Blue, Purple, Magenta, Grey,
+        Pink, Apricot, Beige, Mint, Lavender, White
     };
 
     internal static class AppTheme
@@ -32,6 +34,31 @@ namespace InstantTraceViewerUI
 
         // This color ends up alphablended over the alternating row colors.
         public static uint MatchingRowBgColor;
+
+            uint[] highlightRowBgColors = new uint[Enum.GetValues<HighlightRowBgColor>().Length];
+            highlightRowBgColors[(int)HighlightRowBgColor.Maroon] = 0xFF000080;
+            highlightRowBgColors[(int)HighlightRowBgColor.Brown] = 0xFF24639A;
+            highlightRowBgColors[(int)HighlightRowBgColor.Olive] = 0xFF008080;
+            highlightRowBgColors[(int)HighlightRowBgColor.Teal] = 0xFF909946;
+            highlightRowBgColors[(int)HighlightRowBgColor.Navy] = 0xFF750000;
+            highlightRowBgColors[(int)HighlightRowBgColor.Black] = 0xFF000000;
+            highlightRowBgColors[(int)HighlightRowBgColor.Red] = 0xFF4B19E6;
+            highlightRowBgColors[(int)HighlightRowBgColor.Orange] = 0xFF3182F5;
+            highlightRowBgColors[(int)HighlightRowBgColor.Yellow] = 0xFF19E1FF;
+            highlightRowBgColors[(int)HighlightRowBgColor.Lime] = 0xFF45EFBF;
+            highlightRowBgColors[(int)HighlightRowBgColor.Green] = 0xFF4BB43C;
+            highlightRowBgColors[(int)HighlightRowBgColor.Cyan] = 0xFFF4D442;
+            highlightRowBgColors[(int)HighlightRowBgColor.Blue] = 0xFFD86343;
+            highlightRowBgColors[(int)HighlightRowBgColor.Purple] = 0xFFB41E91;
+            highlightRowBgColors[(int)HighlightRowBgColor.Magenta] = 0xFFE632F0;
+            highlightRowBgColors[(int)HighlightRowBgColor.Grey] = 0xFFA9A9A9;
+            highlightRowBgColors[(int)HighlightRowBgColor.Pink] = 0xFFD4BEFA;
+            highlightRowBgColors[(int)HighlightRowBgColor.Apricot] = 0xFFB1D8FF;
+            highlightRowBgColors[(int)HighlightRowBgColor.Beige] = 0xFFC8FAFF;
+            highlightRowBgColors[(int)HighlightRowBgColor.Mint] = 0xFFC3FFAA;
+            highlightRowBgColors[(int)HighlightRowBgColor.Lavender] = 0xFFffbedc;
+            highlightRowBgColors[(int)HighlightRowBgColor.White] = 0xFFFFFFFF;
+            HighlightRowBgColors = highlightRowBgColors;
 
         static AppTheme()
         {
@@ -73,16 +100,6 @@ namespace InstantTraceViewerUI
 
                 ThreadTimelineLogViewRegionColor = 0xFFC0C0C0;
             }
-
-            int highlightRowBgColorCount = Enum.GetValues<HighlightRowBgColor>().Length;
-            HighlightRowBgColors = Enumerable.Range(0, highlightRowBgColorCount).Select(i =>
-            {
-                int hue = i * (360 / highlightRowBgColorCount);
-
-                // Beware: Higher brightness that 0.6 causes the red color to be too hard to read with error text (ErrorColor).
-                ImGui.ColorConvertHSVtoRGB((hue % 360) / 360.0f, 1.0f /* saturation */, 0.6f /* value/brightness */, out float r, out float g, out float b);
-                return ImGui.GetColorU32(new Vector4(r, g, b, 1.0f));
-            }).ToArray();
 
             InfoColor = ImGui.ColorConvertFloat4ToU32(ImGui.GetStyle().Colors[(int)ImGuiCol.Text]);
             VerboseColor = ImGui.ColorConvertFloat4ToU32(InterpolateColor(0.4f, ImGuiCol.Text, ImGuiCol.WindowBg));
